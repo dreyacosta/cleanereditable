eee596eacd8bb0e3020159d069581795b37dcdb5
David Rey <david.rey.acosta@gmail.com>
2013-12-06 21:32:16 +0100

    Improved cleancontent method and set data-placeholder

--- src/cleanereditable.js
+++ src/cleanereditable.js
@@ -11,46 +11,45 @@
     cleanerEditable = root.cleanerEditable = {};
   }
 
-  cleanerEditable.VERSION = '0.0.1';
+  cleanerEditable.VERSION = '0.0.2';
 
   var cleanContent = cleanerEditable.cleanContent = function(event) {
     var result = [];
     var element = event.currentTarget;
+    var placeholder = element.getAttribute('data-placeholder');
+
+    if (element.innerHTML === '<p><br></p>') {
+      element.innerHTML = '';
+    }
 
     element.innerHTML = element.innerHTML.replace(/__/g, '');
 
-    if (element.innerHTML === '') {
-      var placeholder = element.getAttribute('data-placeholder');
+    if (element.innerHTML === '' || element.innerHTML === placeholder) {
       element.innerHTML = placeholder !== null ? placeholder : 'Edit me';
-    }
+    } else {
+      for (var i = 0; i < element.childNodes.length; i++) {
+        var child = element.childNodes[i];
 
-    for (var i = 0; i < element.childNodes.length; i++) {
-      var child = element.childNodes[i];
+        if (child.textContent) {
+          child.innerHTML = '<p>' + child.textContent + '</p>';
 
-      if (child.innerHTML) {
-        if ( child.getAttribute('class') ) {
-          child.removeAttribute('class');
+          result.push(child.innerHTML);
         }
-
-        if ( child.getAttribute('style') ) {
-          child.removeAttribute('style');
-        }
-
-        child.innerHTML = child.innerHTML.replace(/<.*?>/g, '');
-        child.innerHTML = '<p>' + child.innerHTML + '</p>';
-
-        result.push(child.innerHTML);
       }
-    }
 
-    if (result.length > 0) {
-      element.innerHTML = result.join('');
+      if (result.length > 0) {
+        element.innerHTML = result.join('');
+      }
     }
   };
 
   var emptyEditable = cleanerEditable.emptyEditable = function(argument) {
     var element = event.currentTarget;
     var placeholder = element.getAttribute('data-placeholder');
+
+    if (element.innerHTML === placeholder) {
+      element.innerHTML = placeholder;
+    }
 
     if (element.innerHTML === placeholder || element.innerHTML === 'Edit me') {
       element.innerHTML = '__';
